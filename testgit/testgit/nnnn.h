708f8b35af8996712d8fcd4e0fe71ed44cb8682c
ding <adf@sd.com>
2015-09-23 14:40:34 +0800

    dfddd

---
+++ testgit/testgit/nnnn.h
@@ -0,0 +1,13 @@
+//
+//  nnnn.h
+//  testgit
+//
+//  Created by dingjm on 15/9/23.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface nnnn : UIButton
+
+@end
---
+++ testgit/testgit/nnnn.m
@@ -0,0 +1,21 @@
+//
+//  nnnn.m
+//  testgit
+//
+//  Created by dingjm on 15/9/23.
+//  Copyright (c) 2015年 Apress. All rights reserved.
+//
+
+#import "nnnn.h"
+
+@implementation nnnn
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
